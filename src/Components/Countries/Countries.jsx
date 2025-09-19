bcb4f18b5290e4538711b432e8fc9d95655ba714
jisankhan5 <jk1483777@gmail.com>
2025-09-19 19:35:31 +0600

    fourth commit

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,16 +1,18 @@
 import React, { use } from "react";
 import Country from "../Country/Country";
-
+import './Countries.css'
 const Countries = ({ countriesPromise }) => {
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
 
   return (
-    <div>
+    <div className="text">
       <h1>In The Countries: {countries.length}</h1>
-      {countries.map((country) => (
+    <div className="countries">
+        {countries.map((country) => (
         <Country key={country.cca3.cca3} country={country}></Country>
       ))}
+    </div>
     </div>
   );
 };
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import './Country.css'
 
 const Country = ({ country }) => {
   console.log(country);
 
   return (
-    <div>
+    <div className="country">
       <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
-      <h3>Name: {country.name.common}</h3>
+     <div className="title">
+         <h3>Name: {country.name.common}</h3>
       <h3>Official Name: {country.name.official}</h3>
       <h3>Area: {country.area.area}</h3>
       <h3>Capital: {country.capital.capital[0]}</h3>
       <h3>Region: {country.region.region}</h3>
       <h3>Population: {country.population.population}</h3>
+     </div>
     </div>
   );
 };
